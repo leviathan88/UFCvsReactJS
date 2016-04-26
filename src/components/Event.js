94fede370e67845e7d93ad5b9ab42c30b6247769
leviathan88 <levigithuber@gmail.com>
2016-04-26 10:34:10 +0200

    adding toggle method to event and changing styles

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,13 +2,35 @@ import React, {Component} from 'react';
 import Time from 'react-time'
 
 class Event extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false
+    };
+  }
+
+  toggleDetails() {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
   render() {
+    let eventDetails;
+    if(this.state.showDetails) {
+      eventDetails = (
+        <div>
+          <div><img src={this.props.img}/></div>
+          <div className="event_details" >{this.props.desc}</div>
+        </div>
+      );
+    }
+
     return(
       <div className="event">
-        <div className="event_title">{this.props.arena}</div>
-        <div> <Time value={this.props.event_date} format="YYYY/MM/DD" /></div>
-        <div><img src={this.props.img}/></div>
-        <div className="event_details">{this.props.desc}</div>
+        <div className="event_details" onClick={this.toggleDetails.bind(this)}>
+          {this.props.arena} <p>date:   <Time value={this.props.event_date} format="YYYY/MM/DD" /> </p>
+
+        </div>
+        {eventDetails}
       </div>
     );
   }
--- src/components/example.js
+++
@@ -1,5 +0,0 @@
-<div id="images">
-  <a href="https://facebook.github.io/react/"><img src={react} alt="React" class="versus"/></a>
-  <img src={vs} alt="React" class="versus" id="vs"/>
-  <a href="https://ufc.com"><img src={ufc} alt="UFC" class="versus"/></a>
-</div>
