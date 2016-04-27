f070097d2670e66839d1f680d283ac59d064b556
leviathan88 <levigithuber@gmail.com>
2016-04-27 13:02:18 +0200

    adding a Header component

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, {Component, PropTypes} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <ul className="nav nav-pills pull-right">
+          <li claclassNamess="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h3 className="text-muted">Welcome to {this.props.name}</h3>
+    </div>
+    )
+  }
+}
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+export default Header;
