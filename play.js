6486df857958e4dd89ed73f5c3265d3deac0079f
leviathan88 <levigithuber@gmail.com>
2016-04-29 10:46:00 +0200

    new update methods, just for learning

--- play.js
+++ play.js
@@ -39,3 +39,90 @@ let newTicket = update(originalTicket,{
                          codeshare: {
                            0: { $set: {company:’AZ’, flightNo:’7320’} }
                          }
+
+//delete method using update
+ deleteTask(cardId, taskId, taskIndex){
+   // Find the index of the card
+   let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+   // Create a new object without the task
+   let nextState = update(this.state.cards, {
+     [cardIndex]: {
+       tasks: {$splice: [[taskIndex,1]] }
+     }
+   });
+
+   // set the component state to the mutated object
+   this.setState({cards:nextState});
+
+   // Call the API to remove the task on the server
+   fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+     method: ’delete’,
+     headers: API_HEADERS
+   });
+ }
+
+ //toggle method
+ toggleTask(cardId, taskId, taskIndex){
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+    // Save a reference to the task’s ’done’ value
+    let newDoneValue;
+    // Using the $apply command, you will change the done value to its opposite
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: {
+          [taskIndex]: {
+            done: { $apply: (done) => {
+                newDoneValue = !done
+                return newDoneValue;
+              }
+            }
+          }
+        }
+      }
+    });
+
+    // set the component state to the mutated object
+    this.setState({cards:nextState});
+
+    // Call the API to toggle the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+        method: ’put’,
+        headers: API_HEADERS,
+        body: JSON.stringify({done:newDoneValue})
+    });
+  }
+
+  //add method with update
+  addTask(cardId, taskName){
+      // Find the index of the card
+      let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+      // Create a new task with the given name and a temporary ID
+      let newTask = {id:Date.now(), name:taskName, done:false};
+
+      // Create a new object and push the new task to the array of tasks
+      let nextState = update(this.state.cards, {
+                        [cardIndex]: {
+                          tasks: {$push: [newTask] }
+                        }
+                      });
+
+      // set the component state to the mutated object
+      this.setState({cards:nextState});
+
+      // Call the API to add the task on the server
+      fetch(`${API_URL}/cards/${cardId}/tasks`, {
+        method: ’post’,
+        headers: API_HEADERS,
+        body: JSON.stringify(newTask)
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        // When the server returns the definitive ID
+        // used for the new Task on the server, update it on React
+        newTask.id=responseData.id
+        this.setState({cards:nextState});
+      });
+    }
