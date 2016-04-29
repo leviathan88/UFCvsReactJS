5c20ad708305b711e7fcc64b191e4d229313f103
leviathan88 <levigithuber@gmail.com>
2016-04-29 10:12:37 +0200

    creating EventsAppContainer for events data and serving it to Main.js

---
+++ src/components/ListEvents/EventAppContainer.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import 'whatwg-fetch';
+import List from './List';
+
+const API_URL = './events.json';
+const API_HEADERS = {'Content-Type':'application/json'};
+
+class EventAppContainer extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({events: responseData});
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
+  render() {
+    return(
+      <div className="app row">
+        <div className="col-xs-6">
+          <List title='PPV' events={this.state.events.filter((event)=> event.subtitle==='Live on Pay-Per-View')}/>
+          <List title='FS1 Shows' events={this.state.events.filter((event)=> event.subtitle==='FS1')}/>
+        </div>
+        <div className="col-xs-6">
+          <List title='FS1 Live Shows' events={this.state.events.filter((event)=> event.subtitle==='Live on FS1')}/>
+          <List title='Fight Pass Shows' events={this.state.events.filter((event)=> event.subtitle==='Live on UFC FIGHT PASS')}/>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default EventAppContainer;
