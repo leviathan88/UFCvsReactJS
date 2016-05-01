2c3226470c3f5c009bf07114a7e1168171e06450
leviathan88 <levigithuber@gmail.com>
2016-05-01 13:42:51 +0200

    Adding Framekeys for animation

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -4,10 +4,10 @@ class Jumbo extends Component {
   render() {
     return (
       <div className="jumbotron row">
-        <div id="images" className>
-          <a href="https://facebook.github.io/react/"><img src={this.props.react} alt="React" className="versus"/></a>
+        <div id="images">
+          <a href="https://facebook.github.io/react/"><img src={this.props.react} alt="React" className="versus2"/></a>
           <img src={this.props.vs} alt="React" className="versus" id="vs"/>
-          <a href="https://ufc.com"><img src={this.props.ufc} alt="UFC" className="versus"/></a>
+          <a href="https://ufc.com"><img src={this.props.ufc} alt="UFC" className="versus2"/></a>
         </div>
       </div>
     );
