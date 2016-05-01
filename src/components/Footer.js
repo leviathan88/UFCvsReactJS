a3430e5c230c1ed8b641c4784f88d9a40bae8f93
leviathan88 <levigithuber@gmail.com>
2016-05-01 17:05:23 +0200

    adding a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div id="footer">
+        <p>♥ from <a href="https://github.com/leviathan88">Elvis</a></p>
+      </div>
+    )
+  }
+}
+
+export default Footer;
