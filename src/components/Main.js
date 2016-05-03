9f3b2b1b355a14d343c1155c71696e1a81d440d9
leviathan88 <levigithuber@gmail.com>
2016-05-03 10:32:22 +0200

    add routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,14 @@ require('styles/App.css');
 require('bootstrap/dist/css/bootstrap.css');
 
 import React, {Component} from 'react';
+
 import Header from './Header';
 import Jumbo from './Jumbo';
 import Footer from './Footer';
+
 import EventAppContainer from './ListEvents/EventAppContainer';
+import FightersAppContainer from './SearchFighters/FightersAppContainer';
+import FightApp from './DnDFights/FightsApp';
 
 let react = 'https://assets.toptal.io/uploads/blog/category/logo/291/react.png';
 let name = 'UFC Spy';
@@ -14,7 +18,28 @@ let ufc = 'http://www.mmarecap.com/wp-content/uploads/2009/09/UFC-LOGO-3.jpg';
 let vs = 'https://s3.amazonaws.com/applause-devmktg/2015/12/02/5gs8428lyf_VS.png';
 
 class AppComponent extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      route: window.location.hash.substr(1)
+    }
+  }
+
+  componenetDidMount() {
+    window.addEventListener('hashchange', ()=> {
+      this.setState({
+        route: window.location.hash.substr(1)
+      });
+    });
+  }
+
   render() {
+    let Child;
+    case(this.state.route) {
+      case '/fights': Child=FightApp; break;
+      case '/fighters': Child=FightersAppContainer; break;
+      default: Child=EventAppContainer;
+    }
     return (
       <div className="container index">
         <Header name={name}/>
