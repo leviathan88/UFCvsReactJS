fe5b34f018cabf0f017670168ff01bcb2a02b660
leviathan88 <levigithuber@gmail.com>
2016-05-03 11:42:54 +0200

    excluding Header and using routes directly in Main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React, {Component, PropTypes} from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
         <ul className="nav nav-pills pull-right">
-          <li claclassNamess="active"><a href="#">Home</a></li>
-          <li><a href="#/fights">Fights</a></li>
-          <li><a href="#/fighters">Fighters</a></li>
+          <li className="active"><Link to="#">Home</Link></li>
+          <li><Link to="/fighters">Fighters</Link></li>
+          <li><Link to="/fights">Fights</Link></li>          
         </ul>
         <h3 className="text-muted">Welcome to {this.props.name}</h3>
+        {this.props.children}
     </div>
     )
   }
